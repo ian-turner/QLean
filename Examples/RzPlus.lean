67585afef60b981326ced311e5796155f08a41d2
ian-turner <i.r.turner55@gmail.com>
2026-06-24 02:33:16 -0400

    fix parens issue

--- Examples/RzPlus.lean
+++ Examples/RzPlus.lean
@@ -10,7 +10,7 @@ noncomputable section
 
 /-- `Rz(θ) followed by Rz(φ) is equivalent to Rz(θ + φ)`.
     Proof works by reducing the statement to matrix equality. --/
-theorem rz_plus (θ φ : ℝ) : (RzGate φ * RzGate θ ≈ RzGate θ + φ) := by
+theorem rz_plus (θ φ : ℝ) : (RzGate φ * RzGate θ ≈ RzGate (θ + φ)) := by
   simp [RzGate, QCircuit.Equiv, QCircuit.eval, Rz]
   ring_nf
   simp [Complex.exp_add]
